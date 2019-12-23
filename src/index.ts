b065ae3d1e16c0e6df39e6e70a78648c54161740
rodrigobustamante <rbustamantejelvez@gmail.com>
2019-12-23 00:39:31 -0300

    Reading communes from .env variable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import dotenv from 'dotenv';
 import portalInmobiliario from './spiders/portal-inmobiliario';
 import { connectToDB, disconnectFromDB } from './services/mongo';
+import { splitSeparatedFields } from './utils/helpers';
 
-(async (): Promise <void> => {
+dotenv.config();
+
+(async (): Promise<void> => {
+  const communes = splitSeparatedFields(process.env.COMMUNES);
+
+  if (!communes) {
+    return;
+  }
+
   try {
     await connectToDB();
-    await portalInmobiliario();
+
+    await Promise.all(communes.map(async commune => {
+      await portalInmobiliario(commune);
+    }));
+
     await disconnectFromDB();
   } catch (error) {
     throw new Error(error.message);
