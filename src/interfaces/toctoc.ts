8b53660f4cba954cff775c9796faa993f498ba47
Rodrigo Bustamante <rbustamante@acid.cl>
2019-12-30 21:19:01 -0300

    Add toctoc interface

---
+++ src/interfaces/toctoc.ts
@@ -0,0 +1,41 @@
+export interface CommuneInfo {
+  id: number,
+  name: string,
+  label: string
+}
+
+export interface SearchCommuneResponse {
+  suggest: Suggest
+}
+
+interface Suggest {
+  autocompletesuggest: AutocompleteSuggestion[]
+}
+
+interface AutocompleteSuggestion {
+  text: string,
+  offset: number,
+  length: number,
+  options: SuggestionOptions[]
+}
+
+interface SuggestionOptions {
+  text: string,
+  _index: string,
+  _type: string,
+  _id: string,
+  _score: number,
+  _source: OptionSource
+}
+
+interface OptionSource {
+  suggest: OptionSourceSuggestions[]
+  Texto: string,
+  IdInterno: number,
+  IdTipo: string
+}
+
+interface OptionSourceSuggestions {
+  input: string,
+  weight: number
+}
